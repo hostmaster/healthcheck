fc131b77874a0f0e51e21a27ba9028e5b393245c
Igor Khomyakov <github.site@4ck.me>
2020-03-18 20:50:32 +0300

    add timeout flag

--- health-check.go
+++ health-check.go
@@ -1,12 +1,23 @@
 package main
 
 import (
+	"flag"
 	"net/http"
 	"os"
+	"time"
 )
 
+var timeout time.Duration
+
 func main() {
-	_, err := http.Get(os.Args[1])
+	flag.DurationVar(&timeout, "t", 3*time.Second, "HTTP connection timeout in seconds")
+	flag.Parse()
+
+	c := &http.Client{
+		Timeout: timeout,
+	}
+
+	_, err := c.Get(flag.Arg(0))
 	if err != nil {
 		os.Exit(1)
 	}
